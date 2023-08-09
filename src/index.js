9ff330aaaf36d6fb2496665e0ccc981791fde1e6
Elusai Soares <elusaisoares@gmail.com>
2023-08-09 16:25:28 -0300

    feat: get a feedback

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const jsonToExcel = require('./excel/index')
 
 const main = async (sourcePath) => {
     await jsonToExcel(sourcePath)
+    console.log("Finalizado com Sucesso")
 }
 
 const sourcePath = process.argv[2]
